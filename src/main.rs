a489ff6c7ce549eb60bca17baac01d4cc24c6c3f
Inigo Crespo <inigo.crespo@mail.com>
2023-06-04 20:58:05 +0200

    Adds map to state

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,30 @@
 use std::process;
 
+mod map;
+
 mod prelude {
     pub use bracket_lib::prelude::*;
+    pub const SCREEN_WIDTH: i32 = 80;
+    pub const SCREEN_HEIGHT: i32 = 50;
+    pub use crate::map::*;
 }
 
 use prelude::*;
 
-struct State {}
+struct State {
+    map: Map,
+}
+
+impl State {
+    fn new() -> Self {
+        Self { map: Map::new() }
+    }
+}
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
-        ctx.print(1, 1, "Hello, Bracket Terminal");
+        self.map.render(ctx);
     }
 }
 
@@ -25,7 +38,7 @@ fn main() {
         process::exit(1);
     }
 
-    if let Err(e) = main_loop(context.unwrap(), State {}) {
+    if let Err(e) = main_loop(context.unwrap(), State::new()) {
         eprintln!("Error in main loop: {:#?}", e);
         process::exit(1);
     }
