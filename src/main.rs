f1c8b55a72aaf58ef47fe4cf3721f64652d0d190
Inigo Crespo <inigo.crespo@mail.com>
2023-06-21 20:18:48 +0200

    Adds player's heads-up display and mosters's heath points

--- src/main.rs
+++ src/main.rs
@@ -69,6 +69,8 @@ impl GameState for State {
         ctx.set_active_console(3);
         ctx.cls();
         self.resources.insert(ctx.key);
+        ctx.set_active_console(1);
+        self.resources.insert(Point::from_tuple(ctx.mouse_pos()));
 
         let current_state = self.resources.get::<TurnState>().unwrap().clone();
         match current_state {
@@ -98,7 +100,7 @@ fn main() -> BError {
         .with_font("terminal8x8.png", 8, 8)
         .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
         .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
-        .with_simple_console_no_bg(DISPLAY_WIDTH * 2, DISPLAY_HEIGHT * 2, "terminal8x8.png")
+        .with_simple_console_no_bg(SCREEN_WIDTH * 2, SCREEN_HEIGHT * 2, "terminal8x8.png")
         .build()?;
 
     main_loop(context, State::new())
